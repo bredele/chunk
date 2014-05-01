0d8d1da130ed8c660fb9fd2a4837abd3328d4796
bredele <olivier.wietrich@gmail.com>
2014-05-01 15:47:43 -0600

    clean stuff and refactor

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ var Queue = require('emitter-queue');
  * @api private
  */
 
-var size = 800;
-var range = 1024000;
+var SIZE = 800;
+var RANGE = 1024000;
 
 
 /**
@@ -25,18 +25,27 @@ module.exports = Chunk;
 
 
 /**
- * chunk constructor.
+ * Create chunks from a file
+ * according webrtc datachannel mtu and
+ * app cache.
+ *
+ * @param {Blob|File} file
  * @api public
  */
 
-function Chunk(file, range) {
-  if(!(this instanceof Chunk)) return new Chunk(file, range);
+function Chunk(file) {
+  if(!(this instanceof Chunk)) return new Chunk(file);
   // NOTE: we could may be get better performance with
   // a pool of file reader
-  this.reader = new FileReader();
-  this.file = file;
-  this.total = file.size;
-  if(file) this.read(file, range);
+  var blobs = this.blob(file);
+  var reader = new FileReader();
+  var i = 0;
+  var _this = this;
+  reader.onload = function(event) {
+    _this.chunk(event.target.result, blobs[i++][1]);
+    if(i < blobs.length) reader.readAsDataURL(blobs[i][0]);
+  };
+  reader.readAsDataURL(blobs[i][0]);
 }
 
 
@@ -46,56 +55,52 @@ Emitter(Chunk.prototype);
 Queue(Chunk.prototype);
 
 
-Chunk.prototype.slice = function(begin, end) {
-  var blob = this.file.slice(begin, end);
-  this.reader.readAsDataURL(blob);
+/**
+ * Create chunks from slice. 
+ * The default size is 800 bytes.
+ *
+ * It'll encode the data in order to be reconstruct
+ * by tye callee.
+ *
+ * @param  {Blob | File} blob 
+ * @param  {Numer} index 
+ * @param  {Number} size 
+ * @event chunk
+ * @api private
+ */
+
+Chunk.prototype.chunk = function(blob, index, size) {
+  size = size || SIZE;
+  var l = Math.ceil(blob.length / size);
+  for(var i = 0; i < l; i++) {
+    var start = i * size;
+    var chunk = blob.slice(start, Math.min(start + size, blob.length));
+    this.queue('chunk', chunk, index + i);
+  }
 };
 
-Chunk.prototype.encode = function(binary) {
-};
 
-Chunk.prototype.read = function(file) {
-  var size = this.total;
+/**
+ * Slice file into multiple blobs.
+ * The default size is the app cache capacity.
+ * 
+ * @param  {Blob|File} file  
+ * @param  {Number} range 
+ * @return {Array}
+ * @api private
+ */
+
+Chunk.prototype.blob = function(file, range) {
+  range = range || RANGE;
+  var size = file.size;
   var start = 0;
-  var _this = this;
-  var read = function() {
-    if(size > range) {
-      _this.slice(start, start + range);
-      start = start + range;
-      size = size - range;
-    } else {
-      _this.slice(start, start + size);
-      size = 0;
-    }
-  };
-  this.reader.onload = function(event) {
-    var result = event.target.result;
-    _this.queue('blob', event.target.result);
-    if(size) read();
-  };
-  read();
+  var blobs = [];
+  // NOTE: emit blob could be helpful with worker
+  while(size > range) {
+    blobs.push([file.slice(start, start + range), start]);
+    size = size - range;
+    start = start + range;
+  }
+  blobs.push([file.slice(start, start + size), start]);
+  return blobs;
 };
-
-
-
-  // range = range || 1200;
-  // console.log(file.size);
-  // var blob = file.slice(0, 1200);
-  // this.reader.readAsArrayBuffer(blob);
-
-
-  // console.log(file.size);
-  // range = range || 1200;
-  // var size = file.size;
-  // var start = 0;
-  // var blob;
-  // while(size > range) {
-  //   console.log('while', start, start + range);
-  //   blob = file.slice(start, start + range);
-  //   this.reader.readAsBinaryString(blob);
-  //   size = size - range;
-  //   start = start + range;
-  // }
-  // console.log('reste', start, start + size);
-  // blob = file.slice(start, start + size);
-  // this.reader.readAsArrayBuffer(blob);
